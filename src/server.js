eae38414cf0f58d08966b411b7306e7a9c7c5489
Esha Kar <esha.kar001@gmail.com>
2025-11-03 11:54:34 +0530

    Second Coomit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(rateLimiter);
 
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({
-    origin: "https://backend-7s47.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
