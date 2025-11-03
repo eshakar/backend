baad865c3ce28d59310685e39cfdf4a8af05f50d
Esha Kar <esha.kar001@gmail.com>
2025-11-03 11:45:15 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(rateLimiter);
 
 if (process.env.NODE_ENV !== "production") {
   app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://backend-7s47.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
